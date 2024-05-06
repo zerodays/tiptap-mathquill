0db18898adde49ac9afb7e1a8cbc769d58516303
Žiga Patačko Koderman <ziga@zerodays.dev>
2024-05-06 15:17:18 +0200

    build: init repo and build tools

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tiptap Mathquill
+
+A fresh Tiptap extension.
+
+## Installation
+
+```bash
+pnpm install tiptap-mathquill
+```
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { Extension } from "@tiptap/core";
+
+const TiptapMathquill = Extension.create({
+  name: "TiptapMathquill",
+
+  // TODO: make it work
+});
+
+export { TiptapMathquill };
+
+export default TiptapMathquill;
